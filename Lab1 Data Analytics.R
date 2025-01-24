5a2b18d458156bd4090eeabf3410d3ef27e39ccb
Dasha Polosina <dashapolosina@gmail.com>
2025-01-24 15:04:06 -0500

    Update Lab1 Data Analytics.R

--- Lab1 Data Analytics.R
+++ Lab1 Data Analytics.R
@@ -13,3 +13,54 @@ summary(EPI.new) # stats
 > hist(EPI.new, seq(20., 80., 1.0), prob=TRUE) 
 > lines(density(EPI.new,na.rm=TRUE,bw=1.)) # or try bw=“SJ” 
 > rug(EPI.new)'
+
+fivenum(EPI.new,na.rm=TRUE)
+stem(EPI.new)  
+hist(EPI.new) 
+hist(EPI.new, seq(20., 80., 1.0), prob=TRUE) 
+lines(density(EPI.new,na.rm=TRUE,bw=1.))  
+rug(EPI.new)
+
+boxplot(EPI.new, APO.new)
+hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
+lines(density(EPI.new,na.rm=TRUE,bw=1.))
+rug(EPI.new) 
+
+hist(EPI.new, seq(20., 80., 1.0), prob=TRUE)
+lines(density(EPI.new,na.rm=TRUE,bw="SJ")) 
+rug(EPI.new) 
+
+x<-seq(20,80,1) 
+q<- dnorm(x,mean=42, sd=5,log=FALSE) 
+lines(x,q)
+lines(x,.4*q) 
+q <-dnorm(x,mean=65, sd=5,log=FALSE) 
+lines(x,.12*q) 
+
+plot(ecdf(EPI.new), do.points=FALSE, verticals=TRUE) 
+
+qqnorm(EPI.new)
+qqline(EPI.new) 
+qqplot(rnorm(250), EPI.new, xlab = "Q-Q plot for norm dsn") 
+qqline(EPI.new)
+qqplot(rt(250, df = 5), EPI.new, xlab = "Q-Q plot for t dsn") 
+qqline(EPI.new)
+
+#EPI Old
+NAs <- is.na(EPI.old)
+EPI.old.noNAs <- EPI.old[!NAs]
+
+summary(EPI.old) 
+
+plot(ecdf(EPI.old), do.points=FALSE, verticals=TRUE) 
+
+qqnorm(EPI.old)
+qqline(EPI.old) 
+qqplot(rnorm(250), EPI.old, xlab = "Q-Q plot for norm dsn") 
+qqline(EPI.old)
+qqplot(rt(250, df = 5), EPI.new, xlab = "Q-Q plot for t dsn") 
+qqline(EPI.old)
+
+hist(EPI.old, seq(20., 80., 1.0), prob=TRUE)
+lines(density(EPI.old,na.rm=TRUE,bw="SJ")) 
+rug(EPI.old) 
